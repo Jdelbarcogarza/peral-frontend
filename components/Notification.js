ffc410cdb586fe688fa997b1dfbead29780100ff
Jorge Startop <jorge@thestartop.com>
2022-09-25 04:05:01 -0500

    se agregó lógica del logger

--- components/Notification.js
+++ components/Notification.js
@@ -10,23 +10,26 @@ import {
 
 import WarningIcon from '@mui/icons-material/Warning';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 export const Notification = ({ color, time, status }) => {
   return (
     <>
-      <Box bgColor={'white'} width={'100%'} padding={5}>
-        <Center justifyContent={'center'}>
+      <Box bgColor={'white'} width={'100%'} padding={1}>
+      
+        <Center justifyContent={'space-around'}>
+    
           {
 
             status === 'CRITICO' ? <WarningIcon  sx={{ color: 'red', fontSize: '48px' }} />
-            : <WarningAmberIcon sx={{color: '#ED8936', fontSize: '48px'}}/> 
+            : status === 'NORMAL' ? <CheckCircleIcon sx={{color: 'green', fontSize: '48px'}} /> : <WarningAmberIcon sx={{color: '#ED8936', fontSize: '48px'}}/> 
           }
-          <Spacer/>
+          
           <VStack alignItems={'start'}>
             <Text as='b' fontSize={'lg'}>{status}</Text>
             <Text color={'gray.600'} as={'i'}>{time}</Text>
           </VStack>
-          <Spacer/>
+          
         </Center>
       </Box>
     </>
