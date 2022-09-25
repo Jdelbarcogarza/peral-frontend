d516cbbd98fca7139e6856a96077f522a86fcaa7
Jorge Startop <jorge@thestartop.com>
2022-09-24 20:45:50 -0500

    notificación estática terminada

--- components/Notification.js
+++ components/Notification.js
@@ -1,20 +1,29 @@
 
 import React from 'react'
 import {
-    Box,
-    Center,
+  Box,
+  Center,
+  VStack,
+  Text,
+  Spacer
 } from '@chakra-ui/react'
 
 import WarningIcon from '@mui/icons-material/Warning';
 
-export const Notification = () => {
+export const Notification = ({ color, time, status }) => {
   return (
     <>
-        <Box>
-        <Center>
-            <WarningIcon/>
+      <Box bgColor={'white'} width={'100%'} padding={5  }>
+        <Center justifyContent={'center'}>
+          <WarningIcon sx={{ color: 'red', fontSize: '48px' }} />
+          <Spacer/>
+          <VStack alignItems={'start'}>
+            <Text as='b' fontSize={'lg'}>{status}</Text>
+            <Text color={'gray.600'} as={'i'}>{time}</Text>
+          </VStack>
+          <Spacer/>
         </Center>
-        </Box>
+      </Box>
     </>
   )
 }
