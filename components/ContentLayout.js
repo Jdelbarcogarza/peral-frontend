f1bef4006637d5495a7fe1120f83aa43202f6494
Jorge Startop <jorge@thestartop.com>
2022-09-24 20:59:11 -0500

    se incrementó el padding y se cambió el fondo

--- components/ContentLayout.js
+++ components/ContentLayout.js
@@ -11,7 +11,7 @@ Componente donde va todo el layout de cada tab
 export default function ContentLayout({ children }) {
   return (
    <>
-    <Box bgColor={'teal.300'} height={'100%'} width={'100%'} padding={2}>
+    <Box bgColor={'gray.200'} height={'100%'} width={'100%'} padding={4}>
     {/** TODO: orgnizar para que se vaya a un lado con el grid */}
     {children }
     </Box>
